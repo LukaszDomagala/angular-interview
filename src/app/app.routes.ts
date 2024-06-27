c77ecd8ed3df53a044ef34bc6a588a8f54e722b0
LukaszDomagala <lukasz.domagala@bbh.com>
2024-06-27 10:21:01 +0200

    route guard init: add routes, user service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { UserRole } from './user/user.service';
 
-export const routes: Routes = [];
+export interface RouteData {
+    allowedRole: UserRole;
+}
+
+export const routes: Routes = [
+    {
+        path: '', 
+        component: HomeComponent, 
+        data: { allowedRole: UserRole.User } as RouteData, 
+    },
+    {
+        path: 'admin',
+        component: AdminComponent,
+        data: { allowedRole: UserRole.Admin } as RouteData,
+    },
+];
 
 // TODO: add route guard
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BookListComponent } from './../../book-list/book-list.component';
+import { BookCollectionComponent } from './../../book-collection/book-collection.component';
+import { Store } from '@ngrx/store';
+import { selectAvailableBooks } from './../../book-list/books.selectors';
+import { BooksActions } from './../../book-list/books.actions';
+import { CommonModule } from '@angular/common';
+import { GoogleBooksService } from './../../book-list/books.service';
+import { Book } from './../../book-list/books.model';
+import { selectCollection } from './../../book-collection/collections.selectors';
+import { FormComponent } from './../../form/form.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, BookListComponent, BookCollectionComponent, FormComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+
+  books$ = this.store.select(selectAvailableBooks);
+  bookCollection$ = this.store.select(selectCollection);
+  isLoading$ = this.booksService.isLoading$;
+  
+  constructor(
+    private booksService: GoogleBooksService,
+    private store: Store,
+  ) {}
+  
+  onSearch(query: string) {
+    this.store.dispatch(BooksActions.search({ query }));
+  }
+  
+  onAdd(book: Book) {
+    this.store.dispatch(BooksActions.addBook({ book }));
+  }
+  
+  onRemove(book: Book) {
+    this.store.dispatch(BooksActions.removeBook({ book }));
+  }
+  
+  ngOnInit() {
+    // TODO 1: call GoogleBooksService to get list of books
+    // TODO 2: change to NgRx Effect (dispatch action instead of using service directly)
+    this.store.dispatch(BooksActions.search({ query: 'Harry Potter' }));
+  }
+}
